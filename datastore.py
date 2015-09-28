2b89f08a8a0430a940c102bc725fde0e6ed52ec3
Jeremy Morgan <jeremymorgan@gmail.com>
2015-09-28 07:18:05 -0700

    added instructions, still not working yet

--- datastore.py
+++ datastore.py
@@ -1,6 +1,13 @@
 
 import time
 
+import mysql.connector as mariadb
+
+mariadb_connection = mariadb.connect(user='USERNAME', password='PASSWORD', database='weather')
+
+cursor = mariadb_connection.cursor()
+
+
 readings = [
     {
         'id': 1,
@@ -19,4 +26,12 @@ readings = [
 
 def getReadings(amount):
     return readings
+
+def addReading(reading):
+
+    print reading.TempSensor1
+
+    cursor.execute("INSERT INTO reading (TempSensor1,TempSensor2,TempSensor3,TempSensorAvg,Humidity,Pressure,Altitude,SeaLevelPressure,Lux,TimeStamp) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,NOW())", (reading.TempSensor1,reading.TempSensor2,reading.TempSensor3,reading.TempSensorAvg,reading.Humidity,reading.Pressure,reading.Altitude,reading.SeaLevelPressure,reading.Lux))
+    mariadb_connection.commit()
+    return reading
 
