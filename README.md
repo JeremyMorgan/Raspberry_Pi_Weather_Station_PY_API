647de4c0d0e722ec0356766ceac719ec6cdba80c
Jeremy Morgan <jeremymorgan@gmail.com>
2015-09-28 23:57:48 -0700

    updated with working example

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Raspberry_Pi_Weather_Station_PY_API
-Raspberry PI Weather Station Endpoint using Python/Flask and MariaDB
+# Raspberry Pi Weather Station Python API
+
+<img src="http://i.imgur.com/0fuAdsS.jpg" width="600px">
+
+
+This is an alternative endpoint for the Raspberry Pi Weather Station featured here:
+
+[Raspberry Pi Mini Weather Station](https://github.com/JeremyMorgan/Raspberry_Pi_Weather_Station)
+
+It uses a .Net Web API endpoint in the original design. 
+
+[View The Dashboard Here](http://jeremymorgan.github.io/Raspberry_Pi_Weather_Station/#/)
+
+This is an alternative endpoint Endpoint using Python/Flask and MariaDB on a Linux Server. 
+
+
+This can be run on the Pi itself, or on a [Digital Ocean](https://www.digitalocean.com/?refcode=ca6f81fa42b4) Linux Droplet 
--- app.py
+++ app.py
@@ -3,29 +3,28 @@ from flask import Flask, jsonify, abort, make_response, request, url_for
 import time
 import datastore
 import sys
-import mysql.connector
-
-##!flask/bin/python
+import mysql.connector as mariadb
+import logging
+logging.basicConfig(filename='api.log',level=logging.DEBUG)
 
 from flask.ext.httpauth import HTTPBasicAuth
 
 auth = HTTPBasicAuth()
-
 app = Flask(__name__)
 
 readings = [
     {
         'id': 1,
-        'TempSensor1' : 10,
-        'TempSensor2' : 20,
-        'TempSensor3' : 30,
-        'TempSensorAvg' : 15,
-        'Humidity' : 34,
-        'Pressure' : 1012,
-        'Altitude' : 3.3,
-        'SeaLevelPressure' : 394,
-        'Lux' : 12.304,
-        'TimeStamp' : time.strftime("%c")
+        'TempSensor1': 10,
+        'TempSensor2': 20,
+        'TempSensor3': 30,
+        'TempSensorAvg': 15,
+        'Humidity': 34,
+        'Pressure': 1012,
+        'Altitude': 3.3,
+        'SeaLevelPressure': 394,
+        'Lux': 12.304,
+        'TimeStamp': time.strftime("%c")
     }
 ]
 
@@ -33,13 +32,15 @@ readings = [
 
 @auth.get_password
 def get_password(username):
-    if username == 'miguel':
-        return 'python'
+    if username == 'username':
+        return 'password'
     return None
+
 
 @auth.error_handler
 def unauthorized():
     return make_response(jsonify({'error': 'Unauthorized access'}), 401)
+
 
 ## Function to add URI to each requests
 def make_public_reading(reading):
@@ -57,67 +58,74 @@ def make_public_reading(reading):
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
+
 ## Routes
 ## /weather/api/v1/readings  GET ALL
 @app.route('/weather/api/v1/readings', methods=['GET'])
 @auth.login_required
 def get_tasks():
-#   return jsonify({'readings': [make_public_reading(reading) for reading in readings]})
+    #   return jsonify({'readings': [make_public_reading(reading) for reading in readings]})
     return jsonify({'Readings': datastore.getReadings(2)})
 
 
 @app.route('/weather/api/v1/readings/<int:reading_id>', methods=['GET'])
 def get_task(reading_id):
-
     reading = [reading for reading in readings if reading['id'] == reading_id]
 
     if len(reading) == 0:
         abort(404)
     return jsonify({'reading': reading[0]})
 
+
 @app.route('/weather/api/v1/readings', methods=['POST'])
 def create_reading():
-    #if not request.json or not 'TempSensor1' in request.json:
-        #abort(400)
-    reading = {
-        "Altitude": request.json['Altitude'],
-        "Humidity": request.json['Humidity'],
-        "Lux": request.json['Lux'],
-        "Pressure": request.json['Pressure'],
-        "SeaLevelPressure": request.json['SeaLevelPressure'],
-        "TempSensor1": request.json['TempSensor1'],
-        "TempSensor2": request.json['TempSensor2'],
-        "TempSensor3": request.json['TempSensor3'],
-        "TempSensorAvg": request.json['TempSensorAvg'],
-        "TimeStamp": "Sun Sep 27 17:07:11 2015",
-        'id': readings[-1]['id'] + 1,
-        #'description': request.json.get('description', ""),
-    }
+    status = 0
+    logging.info('Started create_reading()')
+
+# if not request.json or not 'TempSensor1' in request.json:
+# abort(400)
+#reading = [{
+#        "Altitude": request.json['Altitude'],
+#        "Humidity": request.json['Humidity'],
+#        "Lux": request.json['Lux'],
+#        "Pressure": request.json['Pressure'],
+#        "SeaLevelPressure": request.json['SeaLevelPressure'],
+#        "TempSensor1": request.json['TempSensor1'],
+#        "TempSensor2": request.json['TempSensor2'],
+#        "TempSensor3": request.json['TempSensor3'],
+#        "TempSensorAvg": request.json['TempSensorAvg'],
+#        "TimeStamp": "Sun Sep 27 17:07:11 2015",
+#        'id': readings[-1]['id'] + 1,
+        # 'description': request.json.get('description', ""),
+#    }]
+
+    req_json = request.get_json()
 
     try:
 
-        import mysql.connector as mariadb
-
-        mariadb_connection = mariadb.connect(user='web', password='SueBob300', database='weather')
+        mariadb_connection = mariadb.connect(user='username', password='password', database='weather')
 
         cursor = mariadb_connection.cursor()
 
-        #addReading(reading)
-        cursor.execute("INSERT INTO reading (TempSensor1,TempSensor2,TempSensor3,TempSensorAvg,Humidity,Pressure,Altitude,SeaLevelPressure,Lux,TimeStamp) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,NOW())", (reading.TempSensor1,reading.TempSensor2,reading.TempSensor3,reading.TempSensorAvg,reading.Humidity,reading.Pressure,reading.Altitude,reading.SeaLevelPressure,reading.Lux))
+        cursor.execute("INSERT INTO reading (TempSensor1,TempSensor2,TempSensor3,TempSensorAvg,Humidity,Pressure,Altitude,SeaLevelPressure,Lux,TimeStamp) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,NOW())", (req_json['TempSensor1'],req_json['TempSensor2'],req_json['TempSensor3'],req_json['TempSensorAvg'],req_json['Humidity'],req_json['Pressure'],req_json['Altitude'],req_json['SeaLevelPressure'],req_json['Lux']))
+
         mariadb_connection.commit()
 
     except mariadb.Error as error:
-        print("Error: {}".format(error))
+        logging.error("Error: {}".format(error))
+        return jsonify({'status': 'failed'}), 400
 
     except IOError as e:
-        print "I/O error({0}): {1}".format(e.errno, e.strerror)
+        logging.error("I/O error({0}): {1}".format(e.errno, e.strerror))
+        return jsonify({'status': 'failed'}), 400
+
     except:
-        print "Unexpected error:", sys.exc_info()[0]
-        raise
+        logging.error("Unexpected error:", sys.exc_info()[0])
+        return jsonify({'status': 'failed'}), 400
 
-    readings.append(reading)
+    # if all is good
+    return jsonify({'status': 'succeeded'}), 201
 
-    return jsonify({'reading': reading}), 201
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
